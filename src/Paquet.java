67c8ee2ba64cf68149ba2e8cf7aafce868b5f6a9
Boulanger Néo <neo.boulanger2@etu.univ-lorraine.fr>
2025-04-02 14:05:34 +0200

    Modification de la classe Paquet.java

--- src/Paquet.java
+++ src/Paquet.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class Paquet {
 	private ArrayList<Carte> paquet;
 	private BufferedReader src;
+	private static int nbCarte=0;
 
 	public Paquet() throws Exception{
 		this.src=new BufferedReader(new FileReader("../data/cartes.txt"));
@@ -17,5 +18,17 @@ public class Paquet {
 
 			this.paquet.add(carte);
 		}
+	}
+
+	public Carte piocherCarte() {
+		// Génération d'un index pour piocher une carte au hasard
+		int index = Math.ceil(Math.random()*52)+1;
+		Carte carte = this.paquet.get(index);
+		this.paquet.retirerCarte(index);
+
+	}
+
+	public retirerCarte(int index) {
+		this.paquet.remove(index);
 	}
 }
